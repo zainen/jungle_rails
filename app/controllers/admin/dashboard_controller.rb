6256a03083dba351212ecbfab1dc96c0554c9532
zainen <zainensuzuki@gmail.com>
2021-02-04 02:14:08 +0000

    all stock count && # categories in dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,17 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: 'jungle', password: 'book'
-
-
+  
   def show
+  @products = Product.all
+  @category = Category.all
+  @category_length = @category.length
+  count = 0
+  @products.each do |item|
+    count += item.quantity
+  end
+
+
+    @count = count
   end
 end
