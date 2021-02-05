ad8baab8b0f2e552894d544844180bd37f3a50bb
zainen <zainensuzuki@gmail.com>
2021-02-05 20:20:37 +0000

    sale message on home

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,12 @@
 class Sale < ActiveRecord::Base
+
+  # scope :active, -> {where...}
+
+  # Active Record Scope
+  def self.active
+    where('sales.starts_on <= ? AND sales.ends_on >= ?', Date.current, Date.current)
+  end
+
   def finished?
     ends_on < Date.current
   end
