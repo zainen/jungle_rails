aadd43f4bff1ea77c954e76ad4755fafe4ac870c
zainen <zainensuzuki@gmail.com>
2021-02-09 23:21:23 +0000

    add product detail render test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10, 
+        price: 64.99
+      )
+    end
+  end
+  scenario 'navigate to product details page' do
+    visit root_path
+    click_on 'Details', match: :first
+
+    save_screenshot
+    expect(page).to have_css 'img.main-img'
+  end
+end
