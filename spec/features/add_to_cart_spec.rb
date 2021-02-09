67e8739f12925762d828696efcaeedc035fc08f2
zainen <zainensuzuki@gmail.com>
2021-02-09 23:29:55 +0000

    test cart when added

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'see product in cart after added' do
+    visit root_path
+
+    click_on "Add", match: :first
+
+    expect(page.has_content?('My Cart (1)'))
+  end
+end
